af334a998176c781af986abd91b7bdd6eaa7c97d
Zaicheng Qi <vmlinz@gmail.com>
2014-05-20 22:01:40 +0800

    * add some dev dependencies * decouple code for unit tests

--- routes/blog.js
+++ routes/blog.js
@@ -1,43 +1,44 @@
-var express = require('express'),
-    router = express.Router(),
-    articleProvider = require('../db/articleprovider-mongodb').mongolabArticleProvider;
+module.exports = function (articleProvider) {
+    var express = require('express'),
+    router = express.Router();
 
-/* GET new blog page. */
-router.get('/new', function(req, res) {
-	res.render('blog_new.jade', {
-		title: 'New Post'
-	});
-});
-
-// POST a new blog with title and body
-router.post('/new', function(req, res) {
-	articleProvider.save({
-		title: req.param('title'),
-		body: req.param('body')
-	}, function(error, docs) {
-		res.redirect('/');
-	});
-});
-
-router.post('/addComment', function(req, res) {
-    articleProvider.addCommentToArticle(req.param('_id'), {
-        person: req.param('person'),
-        comment: req.param('comment'),
-        created_at: new Date()
-       } , function( error, docs) {
-           res.redirect('/blog/' + req.param('_id'))
-       });
-});
-
-router.get('/:id', function(req, res) {
-	console.log(req.params.id);
-    articleProvider.findById(req.params.id, function(error, article) {
-    	console.log(error);
-        res.render('blog_show.jade', {
-            title: article.title,
-            article: article
+    /* GET new blog page. */
+    router.get('/new', function(req, res) {
+        res.render('blog_new.jade', {
+            title: 'New Post'
         });
     });
-});
 
-module.exports = router;
+    // POST a new blog with title and body
+    router.post('/new', function(req, res) {
+        articleProvider.save({
+            title: req.param('title'),
+            body: req.param('body')
+        }, function(error, docs) {
+            res.redirect('/');
+        });
+    });
+
+    router.post('/addComment', function(req, res) {
+        articleProvider.addCommentToArticle(req.param('_id'), {
+            person: req.param('person'),
+            comment: req.param('comment'),
+            created_at: new Date()
+        } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+    });
+
+    router.get('/:id', function(req, res) {
+        console.log(req.params.id);
+        articleProvider.findById(req.params.id, function(error, article) {
+            console.log(error);
+            res.render('blog_show.jade', {
+                title: article.title,
+                article: article
+            });
+        });
+    });
+
+    return router;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-var express = require('express'),
-    router = express.Router(),
-    articleProvider = require('../db/articleprovider-mongodb').mongolabArticleProvider;
+module.exports = function (articleProvider) {
+	var express = require('express'),
+	router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-    articleProvider.findAll(function(error, docs) {
-    	res.render('index.jade', {
-    		title: 'Blog',
-    		articles:docs
-    	});
-    });
-});
+	/* GET home page. */
+	router.get('/', function(req, res) {
+		articleProvider.findAll(function(error, docs) {
+			res.render('index.jade', {
+				title: 'Blog',
+				articles:docs
+			});
+		});
+	});
 
-module.exports = router;
+	return router;
+}
